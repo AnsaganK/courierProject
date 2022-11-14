dbc8f515512ffdb17d7a5b600932940501d1cba7
ansagan <ansagankabdolla4@gmail.com>
2022-11-14 23:06:06 +0600

    add celery tasks file

---
+++ app/tasks.py
@@ -0,0 +1,6 @@
+from celery import shared_task
+
+
+@shared_task
+def create_cities_for_file_task():
+    pass
--- courierProject/settings.py
+++ courierProject/settings.py
@@ -123,3 +123,9 @@ MEDIA_ROOT = BASE_DIR / MEDIA_URL
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
