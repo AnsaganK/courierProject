576e7e266a98d4e1a171f1ec709fa672bc27e1c7
Ansagan <ansagankabdolla4@gmail.com>
2022-11-21 23:58:34 +0600

    add link for executor hours

---
+++ app/templatetags/general.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='line_entry')
+def line_entry(string1, string2):
+    return True if string1 in string2 else False
